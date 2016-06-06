106021096b589d43654d59bedf03b45eeb8e82f6
Bogdan <bmych.artbrains@gmail.com>
2016-06-06 16:06:40 +0300

    commit

--- src/main/java/com/gabiksoft/webapp/service/impl/ConfirmServiceImpl.java
+++ src/main/java/com/gabiksoft/webapp/service/impl/ConfirmServiceImpl.java
@@ -1,9 +1,19 @@
 package com.gabiksoft.webapp.service.impl;
 
+import com.gabiksoft.webapp.dao.DAO;
 import com.gabiksoft.webapp.entity.Confirm;
 import com.gabiksoft.webapp.service.ConfirmService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConfirmServiceImpl extends GenericSeviceImpl<Confirm> implements ConfirmService {
+
+    private DAO<Confirm> dao;
+
+    @Autowired
+    public ConfirmServiceImpl(DAO<Confirm> dao) {
+        this.dao = dao;
+        this.dao.setEntityClass(Confirm.class);
+    }
 }
--- src/main/java/com/gabiksoft/webapp/service/impl/GenericSeviceImpl.java
+++ src/main/java/com/gabiksoft/webapp/service/impl/GenericSeviceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @Transactional
-public class GenericSeviceImpl<T> implements GenericService<T>{
+public abstract class GenericSeviceImpl<T> implements GenericService<T>{
 
     @Autowired
     protected DAO<T> dao;
